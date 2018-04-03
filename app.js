8e0d8b9f37ae553ff6cdbbc9747d020b58f38c1d
Rob E <rob.edwards101@googlemail.com>
2018-04-04 00:09:38 +0100

    minor semantic changes to js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ function removeTextItemFromListArray(arr) {
 
 function tiltNote(listItem) {
     const tilt = Math.floor((Math.random() * 10) + 1);
+    
     if ( tilt < 5 ) {
         listItem.style.transform = "rotate("+ (360 - tilt) + "deg)"; 
     } else {
@@ -14,8 +15,9 @@ function tiltNote(listItem) {
 
 }
 
-function setColor(listItem) {
+function setNoteColor(listItem) {
     const select = document.getElementsByTagName("select")[0];
+    
     if ( select.value === "Yellow" ) {
         listItem.style.backgroundImage = 'url("../images/yellow-note.png")';
     } else if ( select.value === "Pink" ) {
@@ -39,14 +41,14 @@ function rearrangeNotes() {
     
     if ( firstRowListItems.length < fullRow && secondRowListItems.length > emptyRow ) {
     
-            document.getElementsByClassName("first-row")[0].appendChild(secondRowListItems.shift());
-            rearrangeNotes();    
+        document.getElementsByClassName("first-row")[0].appendChild(secondRowListItems.shift());
+        rearrangeNotes();    
     }
     
     if ( secondRowListItems.length < fullRow && thirdRowListItems.length > emptyRow ) {
 
-            document.getElementsByClassName("second-row")[0].appendChild(thirdRowListItems.shift());
-            rearrangeNotes(); 
+        document.getElementsByClassName("second-row")[0].appendChild(thirdRowListItems.shift());
+        rearrangeNotes(); 
     }
 }
 
@@ -56,7 +58,7 @@ function createNote(row, position, noteText) {
   const p = document.createElement("p");
   
   li = tiltNote(li);
-  li = setColor(li);
+  li = setNoteColor(li);
   
   p.className = "note-text";
   p.innerText = noteText;
@@ -78,11 +80,11 @@ function createNote(row, position, noteText) {
   ul.appendChild(li);
 }
 
-let createButton = document.getElementsByTagName("button")[0];
+const createButton = document.getElementsByTagName("button")[0];
 
 createButton.addEventListener("click", function () {
   const textArea = document.getElementsByClassName("text-fields")[0];
-  const listArray = document.getElementsByTagName("li");
+  const listItemArray = document.getElementsByTagName("li");
   const label = document.getElementsByTagName("label")[0];
   const fullRow = 4;
   const noteCharLimit = 45; 
@@ -92,12 +94,12 @@ createButton.addEventListener("click", function () {
     textArea.value = textArea.value.substr(0, noteCharLimit) + "...";
   }
 
-  if ( listArray.length < fullRow ) {
-      createNote( "first-row", listArray.length, textArea.value );
-  } else if ( listArray.length >= fullRow && listArray.length <= (( fullRow * 2 ) -1 ) ) {
-      createNote( "second-row", listArray.length, textArea.value );
-  } else if (listArray.length >= ( fullRow * 2 ) && listArray.length <= (( fullRow * 3 ) -1 )  ) {
-      createNote("third-row", listArray.length, textArea.value);
+  if ( listItemArray.length < fullRow ) {
+      createNote( "first-row", listItemArray.length, textArea.value );
+  } else if ( listItemArray.length >= fullRow && listItemArray.length <= (( fullRow * 2 ) -1 ) ) {
+      createNote( "second-row", listItemArray.length, textArea.value );
+  } else if (listItemArray.length >= ( fullRow * 2 ) && listItemArray.length <= (( fullRow * 3 ) -1 )  ) {
+      createNote("third-row", listItemArray.length, textArea.value);
   } else {
       label.innerText = "No room on the fridge! Click some notes to delete them and try again";
   }
